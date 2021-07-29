98db0e24e17091fc34f3d40422f6a916c9cfbe6c
Detavius Niblack <85222936+dniblack@users.noreply.github.com>
2021-07-29 04:49:48 -0400

    updated the java file
    Updated the java file to include comments to the lines of code

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,7 +1,8 @@
-function My_Function_Assault() {
-var str = "Welcome to the Apex Legends games."
-var result = str.fontcolor("red")
-document.getElementById("Apex_Legends").innerHTML = result;
+function My_Function_Assault() {                //Defining a function and naming it as this will be used in the 
+                                                // HTML alongside the "getElementById" to retrieve the string//
+var str = "Welcome to the Apex Legends games." //Created the value for the string. This will be duplicated.//
+var result = str.fontcolor("red")              //Using fontcolor to change the color of the string. This will be duplicated and used for two other colors (green and orange)
+document.getElementById("Apex_Legends").innerHTML = result; //Putting the value of result into HTML element with "Apex_Legends" id so that it can be called in the HTML
 }
 
 function My_Function_Support() {
@@ -16,8 +17,8 @@ function My_Function_Defense() {
     document.getElementById("Apex_Legends").innerHTML = result;
     }
 
-function My_Function() {
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("Concatenate").innerHTML = sentence;
-    }
+function My_Function() {                    //Defined a function and named it "My_Function" //
+    var sentence = "I am learning";         //Gave a sentence value to the varaible //
+    sentence += " a lot from this book!";   //Added a concatenate that will be added to the previous line and sentence //
+    document.getElementById("Concatenate").innerHTML = sentence; //Putting the value of result into HTML element with "Concatenate" id //
+}
