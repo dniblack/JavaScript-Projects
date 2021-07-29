18217b19e14d7d13bf41b67ad62e8ae30aae5c8c
Detavius Niblack <85222936+dniblack@users.noreply.github.com>
2021-07-29 03:57:01 -0400

    Added lines of code to index and main.js file
    Added lines of code to include a sentence that once clicked would change from "Click here!" to "I am learning a lot from this book!" which I used a concatenate to create. The other lines feature 3 buttons that change the color of the sentence above it to red, green, or orange

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,23 @@
+function My_Function_Assault() {
+var str = "Welcome to the Apex Legends games."
+var result = str.fontcolor("red")
+document.getElementById("Apex_Legends").innerHTML = result;
+}
+
+function My_Function_Support() {
+    var str = "Welcome to the Apex Legends games."
+    var result = str.fontcolor("green")
+    document.getElementById("Apex_Legends").innerHTML = result;
+}
+
+function My_Function_Defense() {
+    var str = "Welcome to the Apex Legends games."
+    var result = str.fontcolor("orange")
+    document.getElementById("Apex_Legends").innerHTML = result;
+    }
+
+function My_Function() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+    }
