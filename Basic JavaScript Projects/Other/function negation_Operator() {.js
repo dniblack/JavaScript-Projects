a301ea33beebc4c69729183133f8a6de3a16b696
Detavius Niblack <85222936+dniblack@users.noreply.github.com>
2021-08-03 22:14:41 -0400

    Created new folder with index and js file
    Created a new folder to include an html index file alongside a javascript file that includes boolean comparison, typeof operator, and logical operators.

---
+++ Basic JavaScript Projects/Other/function negation_Operator() {.js
@@ -0,0 +1,70 @@
+function negation_Operator() {
+    var value1 = Math.tan(45);
+    var x = 45;
+    document.getElementById("Math").innerHTML = value1;
+}
+
+function negation_Operator() {
+    var pi_val = Math.acos(x);
+    x = 45;
+    document.getElementById("Math").innerHTML = pi_val;
+}
+
+function negation_Operator() {
+    var pi_val = Math.PI(45);
+    x = 45;
+    document.getElementById("Math").innerHTML = x;
+}
+
+function Mult_Math() {
+    var mult_math = 4 * 3 * 18;
+    document.getElementById("Multiplication")
+}
+
+function negation_Operator() {
+    var addition = 10 + 75;
+    document.getElementById("Addition").innerHTML = addition;
+    }
+    
+function mysubFunction() {
+    var subtraction = 8 - 4;
+    document.getElementById("Subtraction").innerHTML = subtraction;
+    }
+    
+function all_Operator() {
+    var simple_Math = 4 * 6 / 2 (1 + 5) * 4 - 2;
+    document.getElementById("Modulus_Operator").innerHTML = simple_Math
+    }
+
+function modulus_Operator() {
+    var simple_Math = 45 % 4;
+    document.getElementById("Simple_Math").innerHTML
+}
+
+window.alert(Math.random());
+
+window.alert(Math.random() * 200)
+
+var x = 45 * 5;
+x++;
+document.write(x);
+
+var x = 35 / 5;
+x--;
+document.write(x);
+
+
+function Mult_Math() {
+    var mult_math = 4 * 3 * 18;
+    document.getElementById("Multiplication").innerHTML = mult_math;
+}
+
+function myadditionOperator() {
+    var addition = 10 + 75;
+    document.getElementById("Addition").innerHTML = addition;
+    }
+    
+function mysubFunction() {
+    var subtraction = 8 - 4;
+    document.getElementById("Subtraction").innerHTML = subtraction;
+    }
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,23 @@
+function not_Function() {
+    document.getElementById("Me").innerHTML = !(20 > 10); // In the booelean comparison, I'm using the logical operator ! to compare both sides of the comparison; the return should be fALSE
+    }
+    
+function not2_Function() {
+    document.getElementById("Me_2").innerHTML = !(5 > 10); // In the booelean comparison, I'm using the logical operator ! to compare both sides of the comparison; the return should be true
+    }
+
+    document.write(2 * 4 * 8 * 16 == 2 * 4 * 8 * 16) // using the double equal signs to show that both sides of the comparison are equal to each other
+    document.write("<br>");                         // line break for organization
+    x = "Jurassic Park released in the year of 199X"; // giving the object "x" a value
+    y = "Jurassic Park released in the year of 199x"; // giving the object "y" a value
+    console.write = (x === y); // Used the triple equal sign to compare data types in values; will return false since 199-'s are different
+    document.write("<br>"); // line break for organization
+    document.write(5 > 4 && 2 < 67); // In the boolean comparison, I'm using the logical operator && to compare both sides of the comparison; the return should be true
+    document.write("<br>"); // line break for organization
+    document.write(115 > 34 || 12 < 5); // In the boolean comparison, I'm using the logical operator || to compare both sides of the comparison; the return should be true
+    document.write("<br>"); // line break for organization
+    document.write(typeof "Breath of the Wild"); // Used the typeof operator to return the data type of the variable; the output should be "string"
+    document.write("<br>"); // line break for organization
+    document.write(25 > 14); // In the boolean comparison, I'm using the less than sign to show 25 is less than 14
+    document.write("<br>"); // line break for organization
+    document.write(72 < 90); // In the boolean comparison, I'm using the less than sign to show 72 is less than 90
